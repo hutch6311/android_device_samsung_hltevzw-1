223dab8b377f473ef8a8d7e871e91dcaab68090c
Shareef Ali <shareefalis@cyanogenmod.org>
2015-10-18 15:00:51 -0400

    hltevzw: update init to c++
    Change-Id: If0ff1dd008d6f4c05e8ca5612e8b70245d73127c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,7 +19,7 @@ TARGET_OTA_ASSERT_DEVICE := SM-N900V,hltevzw
 
 # Init
 TARGET_INIT_VENDOR_LIB := libinit_msm
-TARGET_LIBINIT_DEFINES_FILE := device/samsung/hltevzw/init/init_hlte.c
+TARGET_LIBINIT_DEFINES_FILE := device/samsung/hltevzw/init/init_hlte.cpp
 TARGET_UNIFIED_DEVICE := true
 
 # Partitions
